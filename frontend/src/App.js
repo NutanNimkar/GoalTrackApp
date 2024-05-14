889ed026c3a78538d5bc585c4617879b773de638
NutanNimkar <nutanchandra067@gmail.com>
2024-05-14 03:00:30 -0400

    second frontend commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <div className="pages">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
