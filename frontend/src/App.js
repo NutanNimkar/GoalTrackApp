1def38eec424e75802f62427f56b10dcdb218b72
mli273 <felix.li675@gmail.com>
2024-06-23 23:08:46 -0400

    Felix_UserStatus

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import TaskDetails from './pages/TaskDetails';
+import Login from "./pages/Login"
+import UserSignUp from "./pages/SignUp"
+import Friends from "./pages/Friends.js"
+import "./App.css" 
+
 
 function App() {
   return (
@@ -11,6 +16,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/task" element={<TaskDetails />} />
+          <Route path="/SignUp" element={<UserSignUp/>} />
+          <Route path="/Login" element={<Login/>} />
+          <Route path="/friends" element={<Friends/>} />
         </Routes>
       </div>
       </BrowserRouter>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,11 +2,12 @@ import React from 'react'
 import VerticalNavigation from '../components/VerticalNavigation'
 import Tile from '../components/Tile'
 import PageHeader from '../components/PageHeader'
+import "./Home.css"
 
 
 const Home = () => {
   return (
-    <div>
+    <div className='Home'>
     <PageHeader />
     <VerticalNavigation />
     <Tile title="Create a New Group" description="This is the description for Tile 1." />
