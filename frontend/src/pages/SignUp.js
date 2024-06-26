f80c446e5b09c93d0b37518d1e80904aa100be5e
NutanNimkar <nutanchandra067@gmail.com>
2024-06-26 14:14:58 -0400

    login-page-frontend/backend

---
+++ frontend/src/pages/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserSignUp from '../components/AuthComponents/UserSignUp' 
+
+
+
+const SignUp = () =>{
+  return (
+    <div>
+        <UserSignUp/>
+    </div>
+  )
+}
+
+export default SignUp
