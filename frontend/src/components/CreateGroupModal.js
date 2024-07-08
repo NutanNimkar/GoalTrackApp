afd29393e43442b7fd5405f9453ac3b3c0d70843
VishnuNair1234 <vishnunarayans@hotmail.com>
2024-07-07 21:23:48 -0400

    renamed CreateGroupModal.js file

--- frontend/src/components/CreateGroupModal.js
+++ frontend/src/components/CreateGroupModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
-import CreateGroupForm from './CreateGroupForm';
+import CreateGroupForm from './createGroupForm';
 
 const CreateGroupModal = ({ show, handleClose, handleSave, group }) => {
   const onSave = (data) => {
