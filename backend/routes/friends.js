4bd0e6ab5ace05552f5329bdc128530636cb2b59
NutanNimkar <nutanchandra067@gmail.com>
2024-08-24 16:20:36 -0400

    friends changes added more requests

--- backend/routes/friends.js
+++ backend/routes/friends.js
@@ -1,24 +1,34 @@
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
-const { friendsLookup, sendFriendRequest, getFriendRequest, acceptFriendRequest, addFriends} = require('../controllers/authController');
+const {
+  friendsLookup,
+  sendFriendRequest,
+  getFriendRequest,
+  acceptFriendRequest,
+  addFriends,
+  removeFriend,
+} = require("../controllers/friendsController");
 
 // look up users current friends
-router.get('/current-friends', friendsLookup);
+router.get("/friends/:id", friendsLookup);
 
 // adding friends
-router.post('/add-friends/:id', addFriends );
+router.post("/add-friends/:id", addFriends);
 
 // sending friend request
-router.post('/send-friend-req/:id/:id2', sendFriendRequest);
+router.post("/send-req/:id", sendFriendRequest);
 
-// avaiable friend request
-router.get('/available-friends-req/:id', getFriendRequest);
+// available friend requests
+router.get("/friend-requests/:id", getFriendRequest);
 
 // accepting friend request
-router.post('/accepting-friends/:id/:id2', acceptFriendRequest);
+router.post("/accept-req/:id", acceptFriendRequest);
 
+//remove a friend
+router.delete("/friend/remove/:id", removeFriend);
 
-
+//remove a friend request
+router.delete("/request/remove/:id", removeFriend);
 
 module.exports = router;
