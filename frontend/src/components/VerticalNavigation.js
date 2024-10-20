3b9cdb39a1661c6ad3e3af699ddcf18638132235
mli273 <felix.li675@gmail.com>
2024-10-20 12:50:13 -0400

    Added more responsiveness and new functionality, changed theme

--- frontend/src/components/VerticalNavigation.js
+++ frontend/src/components/VerticalNavigation.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import { useLocation } from 'react-router-dom'
 import './VerticalNavigation.css'
-import Logo from "../Images/Logo-v2.PNG"
+import Logo from "../Images/Logov3.PNG"
 import Group from "../Images/users-group-rounded-svgrepo-com.svg"
 import Home from "../Images/home-alt-3-svgrepo-com.svg"
 import Friends from "../Images/network-1-svgrepo-com.svg"
@@ -29,18 +29,17 @@ return (
   <div className={closeMenu === false ? "vertical-navbar" : "vertical-navbar active"}>
 
 
-
     <div
               className={
                   closeMenu === false
                       ? "Logocontainer"
                       : "Logocontainer active"
               }>
-               <img src={Logo} className = "Logo" alt='Logo'
+               <img src={Logo} className = "Navbar-Logo" alt='Logo'
                 onClick={() => {
                 handleCloseMenu();
                 }} />
-               <h1 className='title'>GOALSEEK</h1>
+               <h1 className='Navbar-title'>GOALSEEK</h1>
     </div>
 
 
@@ -51,15 +50,25 @@ return (
              : "contentsContainer active"
        }>
 
-            <ul className='ListContent'>
-              <li className='ListHome'><img src={Home} className='Homepic' alt='Home'/><a href="/">HOME</a></li>
-              <li ><img src = {Group} className='Grouppic' alt='Group'/><a href="/groups">GROUPS</a></li>
-              <li><img src={Friends} className='Friendspic' alt='Friends'/><a href="/friends">FRIENDS</a></li>
-              <li className='LogoutContainer'onClick={handleClick}><img src ={Logout} className='Logopic' alt='Logout'></img><a>LOGOUT</a></li>
+            <ul className='Navbar-ListContent'>
+              <li className='Navbar-ListItem-Home'><img src={Home} className='Navbar-Homepic' alt='Home'/><a href="/">HOME</a></li>
+              <li className='Navbar-ListItem-Group'><img src = {Group} className='Navbar-Grouppic' alt='Group'/><a href="/groups">GROUPS</a></li>
+              <li className='Navbar-ListItem-Friendspic'><img src={Friends} className='Navbar-Friendspic' alt='Friends'/><a href="/friends">FRIENDS</a></li>
+              <li className='Navbar-ListItem-Logout'onClick={handleClick}><img src ={Logout} className='Navbar-Logopic' alt='Logout'></img><a>LOGOUT</a></li>
             </ul>
     
       
       </div>
+
+      <div className={ 
+       closeMenu === false
+            ? "Navbar-Underline"
+             : "Navbar-Underline active"
+       }>
+        
+        </div> 
+
+
   </div>
 )
 }
